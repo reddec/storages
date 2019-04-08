9f32ca00605adc795124966222b0b2ca61ab4880
Alexander Baryshnikov <dev@baryshnikov.net>
2019-04-08 14:25:45 +0800

    add cli to manipulate storage

--- README.md
+++ README.md
@@ -117,6 +117,45 @@ Relay on storages to detect order of keys.
 
 Cleaning of old keys initiates in `Save()`` method automatically in a same thread.
 
+## CLI access
+
+```go get -v github.com/reddec/storages/cmd/storages```
+
+There is a simple command line wrapper around all currently supported database: `storage`. It provides get, put, del and
+list operations over file, leveldb and redis storage.
+
+Important: empty value implies stream.
+
+Usage:
+
+```
+Usage:
+  storages [OPTIONS] [Command] [key] [Value]
+
+Application Options:
+      --db=[file|leveldb|redis] DB mode (default: file) [$DB]
+  -s, --stream                  Use STDIN as source of value [$STREAM]
+  -0, --null                    Use zero byte as terminator for list instead of new line [$NULL]
+
+File storage params:
+      --file.location=          Root dir to store data (default: ./db) [$FILE_LOCATION]
+
+LevelDB storage params:
+      --leveldb.location=       Root dir to store data (default: ./db) [$LEVELDB_LOCATION]
+
+Redis storage params:
+      --redis.url=              Redis URL (default: redis://localhost) [$REDIS_URL]
+      --redis.namespace=        Hashmap name (default: db) [$REDIS_NAMESPACE]
+
+Help Options:
+  -h, --help                    Show this help message
+
+Arguments:
+  Command:                      what to do (put, list, get, del)
+  key:                          key name
+  Value:                        Value to put if stream flag is not enabled
+
+```
 
 ## CLI generators
 
---
+++ cmd/storages/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"github.com/jessevdk/go-flags"
+	"github.com/pkg/errors"
+	"github.com/reddec/storages"
+	"github.com/reddec/storages/filestorage"
+	"github.com/reddec/storages/leveldbstorage"
+	"github.com/reddec/storages/redistorage"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type source interface {
+	Build() (storages.Storage, error)
+}
+
+type fileParams struct {
+	Location string `long:"location" env:"LOCATION" description:"Root dir to store data" default:"./db"`
+}
+
+func (fp *fileParams) Build() (storages.Storage, error) {
+	return filestorage.NewDefault(fp.Location), nil
+}
+
+type levelDbParams struct {
+	Location string `long:"location" env:"LOCATION" description:"Root dir to store data" default:"./db"`
+}
+
+func (ld *levelDbParams) Build() (storages.Storage, error) {
+	return leveldbstorage.New(ld.Location)
+}
+
+type redisParams struct {
+	URL       string `long:"url" env:"URL" description:"Redis URL" default:"redis://localhost"`
+	Namespace string `long:"namespace" env:"NAMESPACE" description:"Hashmap name" default:"db"`
+}
+
+func (rd *redisParams) Build() (storages.Storage, error) {
+	return redistorage.New(rd.Namespace, rd.URL)
+}
+
+var config struct {
+	Db      string        `long:"db" env:"DB" description:"DB mode" default:"file" choice:"file" choice:"leveldb" choice:"redis"`
+	Stream  bool          `long:"stream" short:"s" env:"STREAM" description:"Use STDIN as source of value"`
+	Null    bool          `long:"null" short:"0" env:"NULL" description:"Use zero byte as terminator for list instead of new line"`
+	File    fileParams    `group:"File storage params" namespace:"file" env-namespace:"FILE"`
+	LevelDB levelDbParams `group:"LevelDB storage params" namespace:"leveldb" env-namespace:"LEVELDB"`
+	Redis   redisParams   `group:"Redis storage params" namespace:"redis" env-namespace:"REDIS"`
+	Args    struct {
+		Command string `description:"what to do (put, list, get, del)" choice:"get" choice:"put" choice:"list" choice:"ls" choice:"del" default:"list" required:"yes"`
+		Key     string `description:"key name" positional-arg-name:"key"`
+		Value   string `description:"Value to put if stream flag is not enabled"`
+	} `positional-args:"yes"`
+}
+
+func main() {
+	_, err := flags.Parse(&config)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	var src source
+	switch config.Db {
+	case "file":
+		src = &config.File
+	case "leveldb":
+		src = &config.LevelDB
+	case "redis":
+		src = &config.Redis
+	default:
+		panic("unknown db type: " + config.Db)
+	}
+
+	db, err := src.Build()
+	if err != nil {
+		log.Fatal("failed initialize", config.Db, "db:", err)
+	}
+
+	err = run(db)
+	db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(db storages.Storage) error {
+	var data = []byte(config.Args.Value)
+	if (config.Stream || len(data) == 0) && config.Args.Command == "put" {
+		v, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+		data = v
+	}
+	switch config.Args.Command {
+	case "put", "set":
+		return db.Put([]byte(config.Args.Key), data)
+	case "get":
+		v, err := db.Get([]byte(config.Args.Key))
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write(v)
+		return nil
+	case "list", "ls":
+		return db.Keys(func(key []byte) error {
+			os.Stdout.Write(key)
+			if config.Null {
+				os.Stdout.Write([]byte{0})
+			} else {
+				os.Stdout.Write([]byte("\n"))
+			}
+			return nil
+		})
+	default:
+		return errors.New("Unknown command " + config.Args.Command)
+	}
+}
